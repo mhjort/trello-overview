96098526b379479452aec86898ff8bfd1102764f
Markus Hjort <markus.hjort@gmail.com>
2016-01-11 19:25:19 +0200

    Implement first version of UI

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
+                 [tailrecursion/cljson "1.0.7"]
                  [cljs-http "0.1.39"]
                  [org.clojure/core.async "0.2.374"]
                  [reagent "0.5.0"]
--- src/trello_overview/core.cljs
+++ src/trello_overview/core.cljs
@@ -1,6 +1,7 @@
 (ns trello-overview.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as reagent :refer [atom]]
+            [tailrecursion.cljson :refer [clj->cljson cljson->clj]]
             [cljs.core.async :refer [<!]]
             [cljs-http.client :as http]))
 
@@ -10,14 +11,41 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:boards []}))
+(defonce app-state (atom {:cards []}))
 
-(defn boards []
-  [:h2 "boards"])
-(go
-  (let [response (<! (http/get "/boards"))]
-    (reagent/render-component [boards]
-                              (.-body js/document))))
+(defn- json-parse [s]
+  (js->clj (cljson->clj s) :keywordize-keys true))
+
+(defn cards []
+  [:ul
+   (for [item (:cards @app-state)]
+     ^{:key item} [:li item])])
+
+(defn- get-cards [list-name]
+  (go
+    (let [body (:body (<! (http/get (str "/cards/" list-name))))]
+      (swap! app-state assoc :cards (:cards (json-parse body))))))
+
+(def lists ["Backlog" "In Progress" "Pending Deployment"])
+
+(defn navi []
+  [:div
+   (for [l lists]
+     [:span {:class "navi"
+             :style {:color (if (= (:list @app-state) l)
+                              "red"
+                              "black")}
+             :on-click #(do
+                          (swap! app-state assoc :list l)
+                          (get-cards l))} l])])
+
+(defn trello-app []
+  [:div
+   (navi)
+   (cards)])
+
+(reagent/render-component [trello-app]
+                          (.-body js/document))
 
 
 (defn on-js-reload []
