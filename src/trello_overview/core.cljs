d3fec1deda32101abfd98e2cc0a86d567287e372
Markus Hjort <markus.hjort@gmail.com>
2016-01-12 19:10:01 +0200

    Cleanup

--- src/trello_overview/core.cljs
+++ src/trello_overview/core.cljs
@@ -7,10 +7,6 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
-;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:cards []}))
 
 (defn- json-parse [s]
